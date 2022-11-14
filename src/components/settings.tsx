652d230d0e671307126b8fc068195ba773654435
hdito <hditow@gmail.com>
2022-11-14 15:00:58 +0300

    Change file structure

---
+++ src/components/settings.tsx
@@ -0,0 +1,93 @@
+import { useState } from "react";
+import { IoSettingsSharp } from "react-icons/io5";
+import Switch from "react-switch";
+import styles from "../styles/Settings.module.scss";
+import { SettingsField } from "./SettingsField";
+
+export const Settings = ({
+  focusTime,
+  breakTime,
+  longBreakTime,
+  autoplay,
+  onChangeFocusTime,
+  onChangeBreakTime,
+  onChangeLongBreakTime,
+  onChangeAutoplay,
+}: {
+  focusTime: number;
+  breakTime: number;
+  longBreakTime: number;
+  autoplay: boolean;
+  onChangeFocusTime: (focusTime: number) => void;
+  onChangeBreakTime: (breakTime: number) => void;
+  onChangeLongBreakTime: (longBreakTime: number) => void;
+  onChangeAutoplay: (autoplay: boolean) => void;
+}) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const fields = [
+    {
+      title: "Focus time",
+      name: "focusTime",
+      initValue: focusTime,
+      onChange: onChangeFocusTime,
+    },
+    {
+      title: "Break time",
+      name: "breakTime",
+      initValue: breakTime,
+      onChange: onChangeBreakTime,
+    },
+    {
+      title: "Lonk break time",
+      name: "longBreakTime",
+      initValue: longBreakTime,
+      onChange: onChangeLongBreakTime,
+    },
+  ];
+
+  return (
+    <>
+      <button onClick={() => setIsOpen(true)} className={styles.settings}>
+        <IoSettingsSharp fill="inherit" title="Settings" />
+      </button>
+      {isOpen && (
+        <div
+          className={styles.shade}
+          onClick={(e) => {
+            if (
+              e.target instanceof Element &&
+              e.target.closest(`.${styles.popup}`)
+            )
+              return;
+            setIsOpen(false);
+          }}
+        >
+          <div className={styles.popup}>
+            <h1>Settings</h1>
+            {fields.map((field) => (
+              <SettingsField
+                key={field.name}
+                title={field.title}
+                name={field.name}
+                onChange={field.onChange}
+                initValue={field.initValue}
+              />
+            ))}
+            <label className={styles.switch}>
+              <Switch
+                uncheckedIcon={false}
+                checkedIcon={false}
+                checked={autoplay}
+                onChange={(checked) => onChangeAutoplay(checked)}
+              />
+              Autoplay
+            </label>
+            <button onClick={() => setIsOpen(false)} className={styles.close}>
+              Close
+            </button>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
