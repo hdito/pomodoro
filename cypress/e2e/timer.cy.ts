72c417b63b6b03a3d4ff2dc315bb613cb45bce67
hdito <hditow@gmail.com>
2022-12-09 15:47:06 +0300

    Remove custom timers from tests

--- cypress/e2e/timer.cy.ts
+++ cypress/e2e/timer.cy.ts
@@ -2,45 +2,14 @@ import { MS_IN_MINUTE, MS_IN_SECOND } from "./../../src/utils/constants";
 import "@testing-library/cypress/add-commands";
 
 describe("Timer", () => {
-  it("Switch modes after timer runs out", () => {
-    cy.clock(new Date().getTime(), ["setInterval"]);
+  it("Timer ticks", () => {
     cy.visit("");
+
     cy.findByRole("timer").contains("25:00");
-    cy.findByRole("button", { name: "Focus" }).should(
-      "have.attr",
-      "aria-current",
-      "step"
-    );
-    cy.findByRole("button", { name: "Break" }).should(
-      "not.have.attr",
-      "aria-current",
-      "step"
-    );
-    cy.findByRole("button", { name: "Long break" }).should(
-      "not.have.attr",
-      "aria-current",
-      "step"
-    );
     cy.findByRole("button", { name: "Start" }).click();
-    cy.tick(12.5 * MS_IN_MINUTE);
-    cy.findByRole("timer").contains("12:30");
-    cy.tick(12.5 * MS_IN_MINUTE);
-    cy.findByRole("button", { name: "Focus" }).should(
-      "not.have.attr",
-      "aria-current",
-      "step"
-    );
-    cy.findByRole("button", { name: "Break" }).should(
-      "have.attr",
-      "aria-current",
-      "step"
-    );
-    cy.findByRole("button", { name: "Long break" }).should(
-      "not.have.attr",
-      "aria-current",
-      "step"
-    );
-    cy.findByRole("timer").contains("05:00");
+    cy.findByRole("timer").contains("24:59");
+    cy.findByRole("timer").contains("24:58");
+    cy.findByRole("timer").contains("24:57");
   });
 
   it("Update paused time after changing settings", () => {
@@ -63,28 +32,12 @@ describe("Timer", () => {
     cy.findByRole("timer").contains("10:00");
   });
 
-  it.only("Resets timer after stop", () => {
-    cy.clock(new Date().getTime(), ["setInterval"]);
+  it("Resets timer after stop", () => {
     cy.visit("");
 
     cy.findByRole("button", { name: "Start" }).click();
-    cy.tick(5 * MS_IN_MINUTE);
-    cy.findByRole("timer").contains("20:00");
+    cy.findByRole("timer").contains("24:59");
     cy.findByRole("button", { name: "Stop" }).click();
     cy.findByRole("timer").contains("25:00");
-
-    cy.findByRole("button", { name: "Settings" }).click();
-    cy.findByRole("spinbutton", { name: "Focus" }).type(
-      "{backspace}{backspace}1"
-    );
-    cy.findByRole("button", { name: "Save" }).click();
-
-    cy.findByRole("timer").contains("01:00");
-    cy.findByRole("button", { name: "Start" }).click();
-
-    cy.tick(MS_IN_SECOND);
-
-    cy.findByRole("button", { name: "Stop" }).click();
-    cy.findByRole("timer").contains("01:00");
   });
 });
